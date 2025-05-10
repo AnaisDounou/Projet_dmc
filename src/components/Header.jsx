62bab0657b565700af20d11eac810157d2b5a222
AnaisDounou <cahathel@gmail.com>
2025-05-11 01:48:19 +0200

    aktive link done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoMenu } from "react-icons/io5";
 import { RxCross2 } from "react-icons/rx";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -11,17 +11,29 @@ function Header() {
         setMenuOpen(!menuOpen);
     };
 
+    const navLinkClass = ({ isActive }) =>
+        `transition duration-150 ${isActive ? "text-yellow-300 underline" : "text-white"}`;
+
     return (
         <header className="max-w-[1800px] m-auto">
             <div className="bg-[#1C4179] flex justify-between items-center px-6 md:px-16 lg:px-28 py-3">
                 <img src="./img/logo_dmc.png" alt="Logo" className="w-18 md:w-20 lg:w-24 xl:w-32" />
                 <nav className="flex items-center">
-                    <ul className="hidden md:flex gap-4 lg:gap-8 text-white text-base xl:text-lg font-medium">
-                        <li><Link to="/">Accueil</Link></li>
-                        <li className="flex gap-2 items-center"><Link to="/solutions">Solutions</Link> <IoIosArrowDown /></li>
-                        <li className="flex gap-2 items-center"><Link to="/secteurs">Secteurs</Link> <IoIosArrowDown /></li>
-                        <li className="flex gap-2 items-center"><Link to="/a_propos">A propos</Link> <IoIosArrowDown /></li>
-                        <li><Link to="/contacts">Contacts</Link></li>
+                    <ul className="hidden md:flex gap-4 lg:gap-8 text-base xl:text-lg font-medium">
+                        <li><NavLink to="/" className={navLinkClass}>Accueil</NavLink></li>
+                        <li className="flex gap-2 items-center">
+                            <NavLink to="/solutions" className={navLinkClass}>Solutions</NavLink>
+                            <IoIosArrowDown className="text-white" />
+                        </li>
+                        <li className="flex gap-2 items-center">
+                            <NavLink to="/secteurs" className={navLinkClass}>Secteurs</NavLink>
+                            <IoIosArrowDown className="text-white" />
+                        </li>
+                        <li className="flex gap-2 items-center">
+                            <NavLink to="/a_propos" className={navLinkClass}>A propos</NavLink>
+                            <IoIosArrowDown className="text-white" />
+                        </li>
+                        <li><NavLink to="/contacts" className={navLinkClass}>Contacts</NavLink></li>
                     </ul>
 
                     {/* Hamburger & Close Icons */}
@@ -35,12 +47,21 @@ function Header() {
 
             {/* Mobile Menu */}
             <div className={`bg-[#1C4179] px-6 md:px-16 lg:px-28 py-6 md:hidden ${menuOpen ? "block" : "hidden"}`}>
-                <ul className="flex flex-col gap-5 lg:gap-8 text-white text-base xl:text-lg font-medium">
-                    <li><Link to="/" onClick={toggleMenu}>Accueil</Link></li>
-                    <li className="flex gap-2 items-center"><Link to="/solutions" onClick={toggleMenu}>Solutions</Link> <IoIosArrowDown /></li>
-                    <li className="flex gap-2 items-center"><Link to="/secteurs" onClick={toggleMenu}>Secteurs</Link> <IoIosArrowDown /></li>
-                    <li className="flex gap-2 items-center"><Link to="/a_propos">A propos</Link> <IoIosArrowDown /></li>
-                    <li><Link to="/contacts" onClick={toggleMenu}>Contacts</Link></li>
+                <ul className="flex flex-col gap-5 lg:gap-8 text-base xl:text-lg font-medium">
+                    <li><NavLink to="/" onClick={toggleMenu} className={navLinkClass}>Accueil</NavLink></li>
+                    <li className="flex gap-2 items-center">
+                        <NavLink to="/solutions" onClick={toggleMenu} className={navLinkClass}>Solutions</NavLink>
+                        <IoIosArrowDown className="text-white" />
+                    </li>
+                    <li className="flex gap-2 items-center">
+                        <NavLink to="/secteurs" onClick={toggleMenu} className={navLinkClass}>Secteurs</NavLink>
+                        <IoIosArrowDown className="text-white" />
+                    </li>
+                    <li className="flex gap-2 items-center">
+                        <NavLink to="/a_propos" onClick={toggleMenu} className={navLinkClass}>A propos</NavLink>
+                        <IoIosArrowDown className="text-white" />
+                    </li>
+                    <li><NavLink to="/contacts" onClick={toggleMenu} className={navLinkClass}>Contacts</NavLink></li>
                 </ul>
             </div>
         </header>
